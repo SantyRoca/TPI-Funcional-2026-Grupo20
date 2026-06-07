d48c2af5a05456840ea3c10f192bf058ff8ede4b
SantyRoca <santiagonicolasroca@gmail.com>
2026-06-07 17:41:22 -0300

    requerimiento 2

---
+++ lisp/core.lisp
@@ -0,0 +1,60 @@
+;; ========================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura (No cambia nada afuera; si le pasas los mismos colores, siempre te devuelve el mismo resultado)
+;; ESTRATEGIA: Función Condicional (Elige que hacer usando una condicion logica de tipo 'if')
+;; IMPACTO: No destructiva (No borra ni altera datos, crea una lista nueva para dar la respuesta)
+;; ========================================================
+(defun transicion (color-actual cambiar-a)
+    (if (validar-transiciones-p color-actual cambiar-a)
+      (list color-actual (format nil "cambiar-a-~a" cambiar-a))
+      (list color-actual 'accion-por-defecto)
+    )
+)
+
+;; ========================================================
+;; FUNCIÓN: validar-transiciones-p
+;; NATURALEZA: Pura (Solo analiza los colores que recibe sin tocar nada de la memoria del sistema)
+;; ESTRATEGIA: Función Predicado (Una funcion de control que solo responde T o NIL)
+;; IMPACTO: No destructiva (Determina el resultado mediante operadores booleanos sin alterar estructuras)
+;; ========================================================
+
+(defun validar-transiciones-p (color-actual cambiar-a)
+    (cond
+        ((and (equal color-actual 'en-rojo) (equal cambiar-a 'verde)) t)
+        ((and (equal color-actual 'en-verde) (equal cambiar-a 'amarillo)) t)
+        ((and (equal color-actual 'en-amarillo) (equal cambiar-a 'rojo)) t)
+        (t nil)
+    )
+)
+
+;; ========================================================
+;; FUNCIÓN: obtener-timestamp
+;; NATURALEZA: Impura (Pregunta la hora a la computadora; como el tiempo corre, cada vez que la llamas da un numero distinto)
+;; ESTRATEGIA: Función simple (Hace cuentas matematicas basicas)
+;; IMPACTO: No destructiva (Hace calculos con numeros sueltos sin romper ni modificar variables del programa)
+;; ========================================================
+
+(defun obtener-timestamp ()
+	(- (get-universal-time)
+		;segundos minutos horas fecha mes anio
+		(encode-universal-time 0 0 0 1 1 1970)
+	)
+)
+
+;; ========================================================
+;; FUNCIÓN: timer
+;; NATURALEZA: Pura (recibe un timestamp y devuelve el color del semaforo correspondiente a ese tiempo)
+;; ESTRATEGIA: Función Condicional (Usa un 'cond' para revisar en que tramo de segundos cae el tiempo y decidir el color)
+;; IMPACTO: No destructiva (Crea una variable temporal con 'let' que desaparece apenas la función termina su trabajo)
+;; ========================================================
+
+(defun timer (ts)
+	(let ((tiempo (mod ts 216) ))
+		(cond
+			((<= tiempo 90 ) 'rojo)
+			((<= tiempo 96) 'amarillo)
+			(t 'verde)
+		)
+	)
+)
+
